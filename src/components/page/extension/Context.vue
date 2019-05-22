9a37591ed8781b6a34cab9e728c647cbaaf4d5c7
ZhuYunlong <920200256@qq.com>
2019-05-22 18:59:04 +0800

    添加推广内容图片

--- src/components/page/extension/Context.vue
+++ src/components/page/extension/Context.vue
@@ -15,7 +15,10 @@
                         <el-table-column prop="group_name" width="150"></el-table-column>
                         <el-table-column>
                             <template slot-scope="scope">
-                                <span class="message-title" @click="handleEdit(scope.row)">{{scope.row.content}}</span>
+                                <span class="message-title" @click="handleEdit(scope.row)">
+                                    <span v-if="scope.row.type===1">{{scope.row.content}}</span>
+                                    <img class="show-image" v-if="scope.row.type===2" :src="imgPre +scope.row.content" alt="">
+                                </span>
                             </template>
                         </el-table-column>
                         <el-table-column prop="update_time" width="180"></el-table-column>
@@ -37,7 +40,12 @@
                             <el-table-column prop="group_name" width="150"></el-table-column>
                             <el-table-column>
                                 <template slot-scope="scope">
-                                    <span class="message-title" @click="handleEdit(scope.row)">{{scope.row.content}}</span>
+                                    <span class="message-title" @click="handleEdit(scope.row)">
+                                        <span class="message-title" @click="handleEdit(scope.row)">
+                                            <span v-if="scope.row.type===1">{{scope.row.content}}</span>
+                                            <img class="show-image" v-if="scope.row.type===2" :src="imgPre +scope.row.content" alt="">
+                                        </span>
+                                    </span>
                                 </template>
                             </el-table-column>
                             <el-table-column prop="update_time" width="150"></el-table-column>
@@ -55,7 +63,12 @@
                             <el-table-column prop="group_name" width="150"></el-table-column>
                             <el-table-column>
                                 <template slot-scope="scope">
-                                    <span class="message-title" @click="handleEdit(scope.row)">{{scope.row.content}}</span>
+                                    <span class="message-title" @click="handleEdit(scope.row)">
+                                        <span class="message-title" @click="handleEdit(scope.row)">
+                                            <span v-if="scope.row.type===1">{{scope.row.content}}</span>
+                                            <img class="show-image" v-if="scope.row.type===2" :src="imgPre + scope.row.content" alt="">
+                                        </span>
+                                    </span>
                                 </template>
                             </el-table-column>
                             <el-table-column prop="update_time" width="150"></el-table-column>
@@ -78,9 +91,30 @@
                         <el-option v-for="group in extensionGroups"  :key="group.id" :label="group.name" :value="group.id"></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="内容">
-                    <el-input type="textarea" rows="5" v-model="form.content"></el-input>
+                <el-form-item label="类型">
+                    <el-radio-group v-model="form.type">
+                        <el-radio :label="1">文本</el-radio>
+                        <el-radio :label="2">图片</el-radio>
+                    </el-radio-group>
                 </el-form-item>
+                <el-form-item label="内容">
+                    <el-input v-show='form.type===1' type="textarea" rows="5" v-model="form.content"></el-input>
+
+                    <div v-show="form.type===2" class="crop-demo">
+                        <img v-show="form.imgSrc" :src="form.imgSrc" class="pre-img">
+                        <div class="crop-demo-btn">选择图片
+                            <input class="crop-input" type="file" name="image" accept="image/*" @change="setImage"/>
+                        </div>
+                    </div>
+                </el-form-item>
+            
+                <el-dialog title="裁剪图片" :visible.sync="dialogVisible" width="30%">
+                    <vue-cropper ref='cropper' :src="form.imgSrc" :ready="cropImage" :zoom="cropImage" :cropmove="cropImage" style="width:100%;height:300px;"></vue-cropper>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button @click="cancelCrop">取 消</el-button>
+                        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                    </span>
+                </el-dialog>
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="editVisible = false">取 消</el-button>
@@ -91,12 +125,17 @@
 </template>
 
 <script>
+    import VueCropper  from 'vue-cropperjs';
     import { getGroups } from '@/api/group'
     import { getContents, add, edit, changeStatus } from '@/api/extensionContext'
+
     export default {
         name: 'tabs',
         data() {
             return {
+                imgPre: 'http://192.168.136.129/Public/images/',
+                imgSrc: '',
+                dialogVisible: false,
                 message: 'first',
                 showHeader: false,
                 editVisible: false,
@@ -106,6 +145,9 @@
                 draft: [],
                 recycle: []
             }
+        },
+        components: {
+            VueCropper
         },
         created() {
             this.resetForm()
@@ -117,8 +159,10 @@
                 this.form = {
                     id: 0,
                     content: '',
+                    type: 1,
                     group_id: '',
-                    group_name: ''
+                    group_name: '',
+                    imgSrc: ''
                 }
                 this.idx = -1
             },
@@ -161,6 +205,10 @@
             },
             handleEdit(row) {
                 this.form = row
+                if (this.form.type === 2) {
+                    this.form.imgSrc = this.imgPre + this.form.content
+                    this.form.oldImg = this.form.content
+                }
                 this.editVisible = true
             },
             changeStatus(row, status, success, fail) {
@@ -181,6 +229,9 @@
                 if (this.form.id > 0) {
                     //编辑
                     edit(this.form).then(response => {
+                        if (response) {
+                            this.form.content = response
+                        }
                         this.form.group_name = this.getGroupNameByGroupId(this.form.group_id)
                         let arr
                         switch(this.form.status) {
@@ -188,14 +239,14 @@
                                 arr = this.draft
                                 break;
                             case 1:
-                                arr = this.normal.push(element)
+                                arr = this.normal
                                 break;
                             case 2:
-                                arr = this.recycle.push(element)
+                                arr = this.recycle
                         }
-                        arr.forEach(element => {
+                        arr.forEach((element, index) => {
                             if( element.id === this.form.id) {
-                                element = this.form
+                                arr[index] = this.form
                             }
                         })
                         this.editVisible = false
@@ -249,6 +300,41 @@
                 .catch(error => {
                     console.log(error)
                 })
+            },
+            setImage(e){
+                const file = e.target.files[0];
+                if (!file.type.includes('image/')) {
+                    return;
+                }
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    this.dialogVisible = true;
+                    this.form.imgSrc = event.target.result;
+                    this.imgSrc = this.form.imgSrc
+                    // this.content = this.imgPre
+                    this.$refs.cropper && this.$refs.cropper.replace(event.target.result);
+                };
+                reader.readAsDataURL(file);
+            },
+            cropImage () {
+                this.form.imgSrc = this.$refs.cropper.getCroppedCanvas().toDataURL();
+                // this.form.content = this.form.imgSrc
+            },
+            cancelCrop(){
+                this.dialogVisible = false;
+                this.form.imgSrc = this.imgSrc
+                if (this.form.type === 2) {
+                    this.form.content = this.imgSrc
+                }
+            },
+            imageuploaded(res) {
+                console.log(res)
+            },
+            handleError(){
+                this.$notify.error({
+                    title: '上传失败',
+                    message: '图片上传接口上传失败，可更改为自己的服务器接口'
+                });
             }
         },
         computed: {
@@ -266,6 +352,49 @@
 }
 .handle-row{
     margin-top: 30px;
+}
+ .content-title{
+    font-weight: 400;
+    line-height: 50px;
+    margin: 10px 0;
+    font-size: 22px;
+    color: #1f2f3d;
+}
+.pre-img{   
+    width: 100px;
+    height: 100px;
+    background: #f8f8f8;
+    border: 1px solid #eee;
+    border-radius: 5px;
+}
+.crop-demo{
+    display: flex;
+    align-items: flex-end;
+}
+.crop-demo-btn{
+    position: relative;
+    width: 100px;
+    height: 40px;
+    line-height: 40px;
+    padding: 0 20px;
+    margin-left: 30px;
+    background-color: #409eff;
+    color: #fff;
+    font-size: 14px;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+.crop-input{
+    position: absolute;
+    width: 100px;
+    height: 40px;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    cursor: pointer;
+}
+.show-image {
+    height: 180px;
 }
 
 </style>
